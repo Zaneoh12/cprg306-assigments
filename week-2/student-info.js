519104f7bbacbe3f8a55eae89ef866a9c86f77b1
Zaneoh12 <152631366+Zaneoh12@users.noreply.github.com>
2024-09-24 22:38:30 -0600

    Assignment 2 changes

---
+++ week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+export default function StudentInfo(){
+  return(
+    <div>
+      <p>Zane Hagen</p>
+      <p>
+        <Link href= "https://github.com/Zaneoh12">https://github.com/Zaneoh12</Link>
+      </p>
+    </div>
+  );
+}
